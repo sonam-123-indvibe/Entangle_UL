2ca8c4b9276a0913b3fe4936f7ec0e83573a1bd5
Razvifaizan <razvifaizan0@gmail.com>
2025-05-16 14:43:17 -1100

    test

--- src/Components/Course.jsx
+++ src/Components/Course.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import "../assets/home.css";
+import "../assets/Home.css";
 
 import { motion } from "framer-motion";
 import { useNavigate } from "react-router-dom";
