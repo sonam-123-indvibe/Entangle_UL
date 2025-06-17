a44c50eead6de2b166963600b7137fe1f7406886
sonam-123-indvibe <88430627+sonam-123-indvibe@users.noreply.github.com>
2025-06-17 13:39:13 +0530

    Update TieupCompanies.jsx

--- src/Components/TieupCompanies.jsx
+++ src/Components/TieupCompanies.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
-import '../assets/TieupCompanies.css';
+import '../assets/Style/TieupCompanies.css';
 
 const companies = [
-  { name: 'TCS', logo: 'https://logo.clearbit.com/tcs.com' },
-  { name: 'Infosys', logo: 'https://logo.clearbit.com/infosys.com' },
-  { name: 'Wipro', logo: 'https://logo.clearbit.com/wipro.com' },
-  { name: 'Accenture', logo: 'https://logo.clearbit.com/accenture.com' },
-  { name: 'Tech Mahindra', logo: 'https://logo.clearbit.com/techmahindra.com' },
-  { name: 'HCL', logo: 'https://logo.clearbit.com/hcltech.com' },
+  { name: 'Mind Path Technology Pvt. Ltd.', logo: 'https://www.mindpathtech.com/wp-content/uploads/2025/05/logo-merge-2-1-1536x298.png' },
+  { name: 'YASH Technologies', logo: 'https://logo.clearbit.com/yash.com' },
+  { name: 'Systango Technologies Pvt. Ltd.', logo: 'https://logo.clearbit.com/sYstango.com' },
+  { name: 'FODUU Web Design Company', logo: 'https://logo.clearbit.com/foduu.com' },
+  { name: 'Young Decade IT Software Solution', logo: 'https://logo.clearbit.com/youngdecade.com' },
+  { name: 'Cyber Infrastructure (CIS)', logo: 'https://www.cisin.com/images/xlogo_white_1.png.pagespeed.ic.X1qc7N1L7Y.png' },
+  { name: 'MindCrew Technologies Pvt. Ltd.', logo: 'https://mindcrewtech.com/MindcrewLogo.png' },
+  { name: 'Golden Eagle IT Technologies Pvt. Ltd.', logo: 'https://www.hubdew.com/hubfs/golden-eagle-IT-companies-in-indore.jpg' },
+  { name: 'Parkhya Solutions Pvt. Ltd.', logo: 'https://www.hubdew.com/hubfs/parkhya-IT-companies-in-indore.jpg' },
+  { name: 'Canopus Infosystems Pvt. Ltd.', logo: 'https://www.hubdew.com/hubfs/canopus-IT-companies-in-indore.png' },
+  { name: 'Lemosys Infotech Pvt. Ltd.', logo: 'https://logo.clearbit.com/lemosys.com' },
+  { name: 'Samosys Technologies Pvt. Ltd.', logo: 'https://logo.clearbit.com/samosys.com' },
+  { name: 'Zehntech Technologies Pvt. Ltd.', logo: 'https://www.zehntech.com/wp-content/uploads/2024/11/Header-Logo.svg' },
 ];
+
 
 const TieupCompanies = () => {
   return (
@@ -18,7 +26,7 @@ const TieupCompanies = () => {
         {companies.map((company, index) => (
           <div className="company-card" key={index}>
             <img src={company.logo} alt={company.name} className="company-logo" />
-            <p className='text-dark'>{company.name}</p>
+            <p className='company-name1'>{company.name}</p>
           </div>
         ))}
       </div>
